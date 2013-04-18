e9883cd17cee752f7b76759e404a8c0cdf5f171c
A.J. Brown <aj@flymuch.com>
2013-04-17 21:33:50 -0400

    Adjust logging level.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -83,19 +83,19 @@ log4j = {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
+    error  'org.codehaus.groovy.grails.commons',            // core / classloading
            'org.codehaus.groovy.grails.plugins',            // plugins
            'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 
-    info 'grails.app'
+    info 'grails.app',                 // Logging warnings and higher for all of the app
+            'org.codehaus.groovy.grails.web.servlet',        //  controllers
+            'org.codehaus.groovy.grails.web.pages',          //  GSP
+            'org.codehaus.groovy.grails.web.sitemesh',       //  layouts
+            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+            'org.codehaus.groovy.grails.web.mapping'
 
     debug 'org.codehaus.groovy.grails.orm',
           'org.grails.datastore.gorm.mongo'
