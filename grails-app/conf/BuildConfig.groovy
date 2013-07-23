8915393cb42a8e0425e933ab107e502e3e6e9fce
A.J. Brown <aj@flymuch.com>
2013-07-23 12:06:23 -0400

    Upgrade plugins and application to grails 2.2.3 compatible.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,7 +12,7 @@ grails.project.source.level = 1.6
 //]
 
 grails.plugin.location.'slug-generator' = 'plugins/grails-slug-generator'
-
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -35,54 +35,47 @@ grails.project.dependency.resolution = {
 
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
+        mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
         mavenRepo "http://repo.desirableobjects.co.uk/"
     }
 
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-
-        // runtime 'mysql:mysql-connector-java:5.1.20'
-
         compile 'org.imgscalr:imgscalr-lib:4.2'
         compile 'com.amazonaws:aws-java-sdk:1.4.3'
         compile 'com.cloudinary:cloudinary:1.0.2'
-
         compile 'org.xhtmlrenderer:core-renderer:R8'
         compile 'com.fasterxml.jackson.core:jackson-databind:2.1.2'
+
         test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 
     plugins {
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
-        runtime ":zipped-resources:1.0"
         runtime ":cached-resources:1.0"
-        runtime ":yui-minify-resources:0.1.4"
-        runtime ":twitter-bootstrap:2.2.2"
+        runtime ":jquery:1.8.3"
+        runtime ":resources:1.2.RC2"
+        runtime ":twitter-bootstrap:2.3.2"
+        runtime ":yui-minify-resources:0.1.5"
+        runtime ":zipped-resources:1.0"
 
-
-        compile ':heroku:1.0.1'
-        compile ':cloud-support:1.0.8'
-
-        compile ":font-awesome-resources:3.0"
-        compile ":jquery-ui:1.8.24"
-        compile ":mongodb:1.3.0"
-        compile ':cache:1.0.1'
-        compile ":fields:1.3"
-        compile ":rendering:0.4.3"
+        build ":improx:0.3"
+        build ":tomcat:$grailsVersion"
 
         compile ":ajax-uploader:1.1"
-
         compile ":cache-headers:1.1.5"
+        compile ":fields:1.3"
+        compile ":font-awesome-resources:3.2.1.1"
+        compile ":jquery-ui:1.8.24"
+        compile ":mongodb:1.3.0"
+        compile ":rendering:0.4.4"
+        compile ':cache:1.1.1'
+        compile ':cloud-support:1.0.11'
+        compile ':heroku:1.0.1'
+        compile ":spring-security-core:1.2.7.3"
 
         test(":spock:0.7") {
             exclude "spock-grails-support"
         }
-
-        build ":tomcat:$grailsVersion"
-
     }
 }
